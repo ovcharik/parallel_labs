9c1fdd66206af11f311835a108330789d7be3852
ovcharik <maksim.ovcharik@gmail.com>
2014-09-18 18:15:14 +0600

    Lab 2

---
+++ src/05_1_reduction.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <omp.h>
+
+int main() {
+  int n, sum = 0;
+  std::cin >> n;
+
+  int num_of_threads = 2;
+  omp_set_num_threads(num_of_threads);
+
+  int step = n / num_of_threads;
+
+  #pragma omp parallel reduction(+:sum)
+  {
+    int curr = omp_get_thread_num();
+    for (int i = step * curr; i <= step * (curr + 1); i++) {
+      sum += i;
+    }
+
+    std::stringstream ss;
+    ss << "[" << curr << "]: Sum = " << sum << std::endl;
+    std::cout << ss.str();
+  }
+
+  std::cout << "Sum = " << sum << std::endl;
+  return 0;
+}
