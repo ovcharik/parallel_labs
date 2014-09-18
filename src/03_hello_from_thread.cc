e5c514e7100597ba87ea86f6807b717d35c6bb4a
ovcharik <maksim.ovcharik@gmail.com>
2014-09-18 17:25:10 +0600

    Input data and lab 3

---
+++ src/03_hello_from_thread.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <omp.h>
+
+int main() {
+  int n;
+  std::cin >> n;
+  omp_set_num_threads(n);
+
+  #pragma omp parallel
+  {
+    int curr = omp_get_thread_num();
+    std::stringstream ss;
+    ss << "I am " << curr << " thread from " << n << " threads!" << std::endl;
+    std::cout << ss.str();
+  }
+  return 0;
+}
