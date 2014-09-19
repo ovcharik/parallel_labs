0e496a85be33dec6b6d4b11667d5a155749dd4ed
ovcharik <maksim.ovcharik@gmail.com>
2014-09-19 14:53:49 +0600

    Graphs

--- main.js
+++ main.js
@@ -1,48 +1,93 @@
 $(function () {
+  var currentObj = null;
+  var accFlag = true;
+  var timeFlag = true;
+
+  var getSeries = function(obj) {
+    result = []
+    if (accFlag) {
+      result.push({
+        name: "Acceleration",
+        data: obj['ACC']
+      });
+    }
+    if (timeFlag) {
+      result.push({
+        name: "Time",
+        data: obj['TIME']
+      });
+    }
+    return result;
+  }
+
+  var makeObject = function(text) {
+    var rows = text.split("\n");
+    for (var i in rows) {
+      rows[i] = rows[i].split("\t").map(function(s) {return s.trim();});
+      if (i != 0) {
+        rows[i] = rows[i].map(function(s) {return Number(s);});
+      }
+    }
+    var title = rows.shift();
+    var result = {}
+    for (var i in title) {
+      result[title[i]] = rows.map(function(row){return row[i];});
+    }
+    return result;
+  }
+
+  var makeGraph = function() {
+    a = $('#container').highcharts();
+    if (a) a.destroy();
+
+    if (!currentObj) return;
+
     $('#container').highcharts({
+      title: {
+        text: "Graph"
+      },
+
+      xAxis: {
         title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
+          text: "Threads"
         },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        tooltip: {
-            valueSuffix: '°C'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        categories: currentObj['THR']
+      },
+
+      yAxis: {
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
         }]
+      },
+
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0
+      },
+
+      series: getSeries(currentObj)
     });
+  }
+
+  $("#input").on("paste", function(event) {
+    var $el = $(this);
+    setTimeout(function() {
+      currentObj = makeObject($el.val());
+      makeGraph();
+    }, 0);
+  });
+
+  $("#accFlag").on("change", function() {
+    accFlag = this.checked;
+    makeGraph();
+  });
+
+  $("#timeFlag").on("change", function() {
+    timeFlag = this.checked;
+    makeGraph();
+  });
 });
