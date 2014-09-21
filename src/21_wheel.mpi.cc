91faff97db85630fc92930845a66ce371180a661
ovcharik <maksim.ovcharik@gmail.com>
2014-09-21 16:26:49 +0600

    Fix lab 21

--- src/21_wheel.mpi.cc
+++ src/21_wheel.mpi.cc
@@ -14,12 +14,14 @@ int main(int argc, char ** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   int secret;
-  MPI_Status status;
+  MPI_Status statuses[2];
+  MPI_Request requests[2];
   int s = (rank + 1) % size;
   int r = (rank ? rank : size) - 1;
 
-  MPI_Send(&rank,   1, MPI_INT, s, rank, MPI_COMM_WORLD);
-  MPI_Recv(&secret, 1, MPI_INT, r, r,    MPI_COMM_WORLD, &status);
+  MPI_Isend(&rank,   1, MPI_INT, s, rank, MPI_COMM_WORLD, &(requests[0]));
+  MPI_Irecv(&secret, 1, MPI_INT, r, r,    MPI_COMM_WORLD, &(requests[1]));
+  MPI_Waitall(2, requests, statuses);
 
   std::stringstream ss;
   ss << "[" << rank << "] receive message '" << secret << "'" << std::endl;
